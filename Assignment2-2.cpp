d7880465f3f81ddcbfeedc0bd890380441766d25
aOudanonh <asakoud@gmail.com>
2021-01-25 18:16:49 +0000

    Submit assignment 2-2

---
+++ Assignment2-2.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  //declare and initialize char variable called ltr, set it to character 'A'
+  char ltr = 65;
+  
+  //output statement
+  cout << ltr
+
+  //set ltr to 'r'
+  ltr = 114;
+
+  //output
+  cout << ltr;
+
+  //set ltr to 'n'
+  ltr = 110;
+
+  //output
+  cout << ltr;
+
+  //set ltr to 'o'
+  ltr = 111;
+  
+  //output
+  cout << ltr;
+
+  //set ltr to 'u'
+  ltr = 117;
+
+  //output
+  cout << ltr;
+
+  //set ltr to 's'
+  ltr = 115;
+
+  //output
+  cout << ltr;
+
+  //set ltr to 'a'
+  ltr = 97;
+
+  //output
+  cout << ltr;
+
+  //set ltr to 'k'
+  ltr = 107;
+
+  //output and end line
+  cout << ltr
+       << "\n";
+
+  return 0;
+}
