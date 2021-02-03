ca98cb6f8567aea9aecc69b7e1c98459cd62c5a4
aOudanonh <asakoud@gmail.com>
2021-02-03 17:47:13 +0000

    chapter 3

---
+++ Assignment3-5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+  //seed
+  srand(time(0));
+
+  // declare and initialize variables
+  int num1,
+      num2,
+      num3,
+      sum,
+      avg;
+  
+  
+
+  return 0;
+}
